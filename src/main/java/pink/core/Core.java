4b6558b57779c39a10f792a6bc261401e09530cd
Moseley, Jake <jake66888@gmail.com>
2025-08-27 21:22:31 -0500

    Leave message added

--- src/main/java/pink/core/Core.java
+++ src/main/java/pink/core/Core.java
@@ -3,6 +3,7 @@ package pink.core;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Core extends JavaPlugin {
@@ -14,8 +15,16 @@ public final class Core extends JavaPlugin {
     }
     @EventHandler(priority = EventPriority.LOWEST)
     public void onChat(AsyncPlayerChatEvent event) {
+        event.setCancelled(true);
         event.setFormat("§7[§dPink§7] §f" + event.getPlayer().getName() + " §8» §f" + event.getMessage());
     }
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        event.setQuitMessage("§7[§c-§7] §f" + player.getName());
+    }
+
     @Override
     public void onDisable() {
         getServer().getConsoleSender().sendMessage("§dPink §7| §cPlugin disabled!");
